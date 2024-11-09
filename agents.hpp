57058e611add68b520a58b8992f5565dc6b8e294
Giovanni <giovannimoreton@gmail.com>
2024-11-09 19:14:12 -0300

    a

---
+++ agents.hpp
@@ -0,0 +1,28 @@
+#include "structs.hpp";
+
+struct agetns brim;
+struct agetns phoenix;
+struct agetns sage;
+struct agetns sova;
+struct agetns viper;
+struct agetns cypher;
+struct agetns reyna;
+struct agetns killjoy;
+struct agetns breach;
+struct agetns omen;
+struct agetns jett;
+struct agetns raze;
+struct agetns skye;
+struct agetns yoru;
+struct agetns astra;
+struct agetns kayo;
+struct agetns chamber;
+struct agetns neon;
+struct agetns fade;
+struct agetns harbor;
+struct agetns gekko;
+struct agetns deadlock;
+struct agetns iso;
+struct agetns clove;
+struct agetns vyse;
+
