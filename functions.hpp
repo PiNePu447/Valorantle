275b56319a78c4d3c26f0a1c6bc2b42734019ff3
Giovanni <giovannimoreton@gmail.com>
2024-11-09 20:39:30 -0300

    adicionei metade dos boneco

--- functions.hpp
+++ functions.hpp
@@ -3,8 +3,16 @@
 void declaraBoneco(){
     brim.ep = 0; brim.bang = false; brim.molly = true; brim.role = "controller"; brim.genre = "M"; brim.face = false; brim.wall = false; brim.radiant = false; brim.difficulty = 3;
     phoenix.ep = 0; phoenix.bang = true; phoenix.molly = true; phoenix.role = "duelist"; phoenix.genre = "M"; phoenix.face = false; phoenix.wall = true; phoenix.radiant = true; phoenix.difficulty = 1;
-    sage.ep = 0; sage.bang = false; sage.molly = false; sage.role = "sentinel"; sage.genre = "F"; sage.face = false; sage.wall = true; sage.radiant = true; sage.difficulty = 2;
+    sage.ep = 0; sage.bang = false; sage.molly = false; sage.role = "sentinel"; sage.genre = "F"; sage.face = false; sage.wall = true; sage.radiant = true; sage.difficulty = 1;
     sova.ep = 0; sova.bang = false; sova.molly = false; sova.role = "initiator"; sova.genre = "M"; sova.face = false; sova.wall = false; sova.radiant = false; sova.difficulty = 4;
     viper.ep = 0; viper.bang = false; viper.molly = true; viper.role = "controller"; viper.genre = "F"; viper.face = true; viper.wall = true; viper.radiant = false; viper.difficulty = 5;
-    .ep = ; .bang = ; .molly = ; .role = ; .genre = ; .face = ; .wall = ; .radiant = ; .difficulty = ;
+    cypher.ep = 0; cypher.bang = false; cypher.molly = false; cypher.role = "sentinel"; cypher.genre = "M"; cypher.face = true; cypher.wall = false; cypher.radiant = false; cypher.difficulty = 3;
+    reyna.ep = 1; reyna.bang = true; reyna.molly = false; reyna.role = "duelist"; reyna.genre = "F"; reyna.face = false; reyna.wall = false; reyna.radiant = true; reyna.difficulty = 1;
+    killjoy.ep = 1; killjoy.bang = false; killjoy.molly = true; killjoy.role = "sentinel"; killjoy.genre = "F"; killjoy.face = true; killjoy.wall = false; killjoy.radiant = false; killjoy.difficulty = 2;
+    breach.ep = 0; breach.bang = true; breach.molly = false; breach.role = "initiator"; breach.genre = "M"; breach.face = false; breach.wall = false; breach.radiant = false; breach.difficulty = 3;
+    omen.ep = 0; omen.bang = true; omen.molly = false; omen.role = "controller"; omen.genre = "M"; omen.face = true; omen.wall = false; omen.radiant = true; omen.difficulty = 4;
+    jett.ep = 0; jett.bang = false; jett.molly = false; jett.role = "duelist"; jett.genre = "F"; jett.face = false; jett.wall = false; jett.radiant = true; jett.difficulty = 4;
+    raze.ep = 0; raze.bang = false; raze.molly = false; raze.role = "duelist"; raze.genre = "F"; raze.face = false; raze.wall = false; raze.radiant = false; raze.difficulty = 5;
+    skye.ep = 1; skye.bang = true; skye.molly = false; skye.role = "initiator"; skye.genre = "F"; skye.face = false; skye.wall = false; skye.radiant = true; skye.difficulty = 3;
+    yoru.ep = 2; yoru.bang = true; yoru.molly = true; yoru.role = "duelist"; yoru.genre = "M"; yoru.face = true; yoru.wall = false; yoru.radiant = true; yoru.difficulty = 4;
 }
