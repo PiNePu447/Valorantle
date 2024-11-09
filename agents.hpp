7829976acdc9ed04207b14f670e86b0022eada0d
Giovanni <giovannimoreton@gmail.com>
2024-11-09 19:37:34 -0300

    adicionei declara boneco

--- agents.hpp
+++ agents.hpp
@@ -26,4 +26,6 @@ struct agent iso;
 struct agent clove;
 struct agent vyse;
 
-brim.ep = 0
+void declaraBoneco(){
+    brim.ep = 0; brim.bang = false; brim.molly = true; brim.role = "controlador"; brim.genre = "M"; brim.face = false; brim.wall = false; brim.radiant = false; brim.difficulty = 3;
+}
