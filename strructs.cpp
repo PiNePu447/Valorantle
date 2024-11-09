34db2b370fa8acd9ee73accc281a56631b0b65bc
Gustavo Escobar <gueslopes02@hotmail.com>
2024-11-09 18:33:04 -0300

    Atributos adicionados na estrutura de personagens

--- strructs.cpp
+++ strructs.cpp
@@ -3,10 +3,22 @@ using namespace std;
 
 struct agent
 {
+    /*Lançamento*/
     int ep;
+    /*Tem bang?*/
     bool bang;
+    /*Tem molotov?*/
     bool molly;
+    /*Função*/
     string role;
+    /*Gênero*/
     string genre;
+    /*Acessório na cara*/
     bool face;
+    /*Tem parede?*/
+    bool wall;
+    /*Poderes de radianita*/
+    bool radiant;
+    /*Dificuldade de jogar*/
+    int difficulty;
 };
