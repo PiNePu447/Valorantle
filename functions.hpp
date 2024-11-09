0939b873bf1e7c99d82ad01da56565222e4797a0
Giovanni <giovannimoreton@gmail.com>
2024-11-09 20:16:58 -0300

    ad

--- functions.hpp
+++ functions.hpp
@@ -1,5 +1,10 @@
 #include "agents.hpp"
 
 void declaraBoneco(){
-    brim.ep = 0; brim.bang = false; brim.molly = true; brim.role = "controlador"; brim.genre = "M"; brim.face = false; brim.wall = false; brim.radiant = false; brim.difficulty = 3;
+    brim.ep = 0; brim.bang = false; brim.molly = true; brim.role = "controller"; brim.genre = "M"; brim.face = false; brim.wall = false; brim.radiant = false; brim.difficulty = 3;
+    phoenix.ep = 0; phoenix.bang = true; phoenix.molly = true; phoenix.role = "duelist"; phoenix.genre = "M"; phoenix.face = false; phoenix.wall = true; phoenix.radiant = true; phoenix.difficulty = 1;
+    sage.ep = 0; sage.bang = false; sage.molly = false; sage.role = "sentinel"; sage.genre = "F"; sage.face = false; sage.wall = true; sage.radiant = true; sage.difficulty = 2;
+    sova.ep = 0; sova.bang = false; sova.molly = false; sova.role = "initiator"; sova.genre = "M"; sova.face = false; sova.wall = false; sova.radiant = false; sova.difficulty = 4;
+    viper.ep = 0; viper.bang = false; viper.molly = true; viper.role = "controller"; viper.genre = "F"; viper.face = true; viper.wall = true; viper.radiant = false; viper.difficulty = 5;
+    .ep = ; .bang = ; .molly = ; .role = ; .genre = ; .face = ; .wall = ; .radiant = ; .difficulty = ;
 }
