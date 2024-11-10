3e722a1b8524990f4929c684a998045641b30531
Giovanni <giovannimoreton@gmail.com>
2024-11-09 22:02:25 -0300

    to testando coisas aqui

--- main.cpp
+++ main.cpp
@@ -4,8 +4,9 @@ using namespace std;
 
 int main(){
     caracteriscas();
-    string agente;
+    agent escolhido = agent_picker();
+    agent agente;
     cout << "Qual agente?\n     --";
-    cin >> agente;
+    
     cout << agente.name << "\n" << agente.ep << "\n" << agente.bang << "\n" << agente.molly << "\n" << agente.role << "\n" << agente.genre << "\n" << agente.face << "\n" << agente.wall << "\n" << agente.radiant << "\n" << agente.difficulty;
 }
--- teste.cpp
+++ teste.cpp
@@ -1,15 +1,14 @@
 #include <iostream>
-#include <string>
 #include "functions.hpp"
 using namespace std;
 
 int main(){
-    declaraBoneco();
-    agent picked = picker();
-    cout << picked.ep << endl;
-    cout << picked.bang << endl;
-    cout << picked.difficulty << endl;
-    cout << picked.genre << endl;
-    cout << picked.role << endl;
-    cout << picked.radiant << endl;
+    caracteriscas();
+    agent picked = agent_picker();
+    string resposta;
+
+    cout << picked.name;
+
+    cin >> resposta;
+    
 }
