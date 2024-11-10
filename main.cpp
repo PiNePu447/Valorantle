50807b7a11af09f65f65bb86ad62ee8fb20f8d44
Giovanni <giovannimoreton@gmail.com>
2024-11-10 05:45:33 -0300

    aaaaaaaaaaa

--- main.cpp
+++ main.cpp
@@ -7,6 +7,39 @@ int main(){
     cout << "Qual agente?\n     --";
     caracteriscas();
     agent escolhido = agent_picker();
+
+    cout << escolhido.name;
     agent agente = pega_resposta();
-    cout << agente.name << "\n" << agente.ep << "\n" << agente.bang << "\n" << agente.molly << "\n" << agente.role << "\n" << agente.genre << "\n" << agente.face << "\n" << agente.wall << "\n" << agente.radiant << "\n" << agente.difficulty;
+
+    int life = 5, tentativas = 0;
+
+    
+
+    while (tentativas < life)
+    {
+        tentativas++;
+
+        if (agente.name != escolhido.name){
+            cout << "Errou perdeu uma vida" << endl;
+            if (agente.ep == escolhido.ep)
+            {
+                cout << "Lancou no msm ep" << endl;
+            }else{
+                cout << "Errou o ep" << endl;
+            }
+            
+            
+            //agente.ep agente.bang agente.molly agente.role agente.genre agente.face agente.wall agente.radiant agente.difficulty
+        }else{
+            cout << "ACERTOU!!!!!!!!!\n ERA MESMO O(A) " << escolhido.name;
+            break;
+        }
+        
+    }
+
+    cout << endl << "teste se para o while";
+    
+
 }
+
+//cout << agente.name << "\n" << agente.ep << "\n" << agente.bang << "\n" << agente.molly << "\n" << agente.role << "\n" << agente.genre << "\n" << agente.face << "\n" << agente.wall << "\n" << agente.radiant << "\n" << agente.difficulty;
