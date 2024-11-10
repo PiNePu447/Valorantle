96e6248a9073eaac7d775daed3f1b78894ec4cd7
Gustavo Escobar <gueslopes02@hotmail.com>
2024-11-09 21:23:40 -0300

    alterações de nomes e tals

--- main.cpp
+++ main.cpp
@@ -4,5 +4,5 @@ using namespace std;
 
 int main(){
     declaraBoneco();
-    cout << "Agente: Brimstone" << "\n" << brim.ep << "\n" << brim.bang << "\n" << brim.molly << "\n" << brim.role << "\n" << brim.genre << "\n" << brim.face << "\n" << brim.wall << "\n" << brim.radiant << "\n" << brim.difficulty;
+    cout << brim.name << "\n" << brim.ep << "\n" << brim.bang << "\n" << brim.molly << "\n" << brim.role << "\n" << brim.genre << "\n" << brim.face << "\n" << brim.wall << "\n" << brim.radiant << "\n" << brim.difficulty;
 }
